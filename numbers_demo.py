a029b6bebc1281db82fc19efe985feb944992533
reimagen <whereisgu22@gmail.com>
2025-06-05 18:08:53 -0700

    first commit!

---
+++ numbers_demo.py
@@ -0,0 +1,16 @@
+
+MAX_NUMBER = 50
+
+# def even_numbers():
+#     for i in range(MAX_NUMBER):
+#         if i % 2 == 0:
+#             print(i)
+
+# even_numbers()
+
+def odd_numbers():
+    for i in range(MAX_NUMBER):
+        if i % 2 == 1:
+            print(i)
+
+odd_numbers()
